d58836ffbd91ef33eb59956fd38e0fc71c8ea8c4
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-10-02 20:26:50 +1000

    List employee component - hardcoded data

--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -1,12 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../models/employee.model';
 
 @Component({
-  selector: 'app-list-employees',
+  selector: 'list-employees',
   templateUrl: './list-employees.component.html',
   styleUrls: ['./list-employees.component.css']
 })
 export class ListEmployeesComponent implements OnInit {
 
+  // hardcoding temporarily
+  employees: Employee[] = [
+    {
+      id: 1,
+      name: 'Mark',
+      gender: 'Male',
+      contactPreference: 'Email',
+      email: 'mark@test.com',
+      dateOfBirth: new Date('10/25/1978'),
+      department: 'IT',
+      isActive: true,
+      photoPath: 'assets/images/mark.png'
+    },
+    {
+      id: 2,
+      name: 'Mary',
+      gender: 'Female',
+      contactPreference: 'Phone',
+      phoneNumber: 1234431,
+      dateOfBirth: new Date('11/20/1989'),
+      department: 'HR',
+      isActive: true,
+      photoPath: 'assets/images/mary.png'
+    },
+    {
+      id: 3,
+      name: 'John',
+      gender: 'Male',
+      contactPreference: 'Phone',
+      phoneNumber: 8127121,
+      dateOfBirth: new Date('3/25/1985'),
+      department: 'IT',
+      isActive: false,
+      photoPath: 'assets/images/john.png'
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
