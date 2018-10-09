1fa94cfe0be412c4a5d345b4fc4e5d6e30ed9987
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-10-09 22:39:49 +1100

    Customised ngx-bootstrap datepicker

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Department } from '../models/department.model';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-create-employee',
@@ -11,7 +12,7 @@ export class CreateEmployeeComponent implements OnInit {
 
   gender = 'male';
 
-  departments:  Department[] = [
+  departments: Department[] = [
     { id: 1, name: 'Help Desk' },
     { id: 2, name: 'HR' },
     { id: 3, name: 'IT' },
@@ -21,8 +22,23 @@ export class CreateEmployeeComponent implements OnInit {
   // isActive=false;
   // defaut IT department selected
   department = '3';
-  constructor() {
 
+  // create a property of type Partial<BsDatepickerConfig>
+  datePickerConfig: Partial<BsDatepickerConfig>;
+
+  // default DOB
+  // dateOfBirth: Date = new Date(2018, 0, 30);
+
+  // set containerClass property to the preferred values
+  constructor() {
+    this.datePickerConfig = Object.assign({},
+      {
+        containerClass: 'theme-dark-blue',
+        // showWeekNumbers: true,
+        // minDate: new Date(2018, 0, 1),
+        // maxDate: new Date(2018, 11, 31),
+        dateInputFormat: 'DD/MM/YYYY'
+      });
   }
 
   ngOnInit() {
