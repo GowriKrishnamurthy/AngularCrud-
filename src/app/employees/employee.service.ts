713e2639072fff694e083cf82aca50f8df30bc7b
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-10-24 19:28:41 +1100

    Ang service for list emp. dep injection in list emp class to fetch emp details from service

---
+++ src/app/employees/employee.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Employee } from '../models/employee.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  constructor() { }
+  // hardcoding temporarily
+  private listEmployees: Employee[] = [
+    {
+      id: 1,
+      name: 'Mark',
+      gender: 'Male',
+      contactPreference: 'Email',
+      email: 'mark@test.com',
+      dateOfBirth: new Date('10/25/1978'),
+      department: 'IT',
+      isActive: true,
+      picPath: 'assets/images/mark.png'
+    },
+    {
+      id: 2,
+      name: 'Mary',
+      gender: 'Female',
+      contactPreference: 'Phone',
+      phoneNumber: 1234431,
+      dateOfBirth: new Date('11/20/1989'),
+      department: 'HR',
+      isActive: true,
+      picPath: 'assets/images/mary.png'
+    },
+    {
+      id: 3,
+      name: 'John',
+      gender: 'Male',
+      contactPreference: 'Phone',
+      phoneNumber: 8127121,
+      dateOfBirth: new Date('3/25/1985'),
+      department: 'IT',
+      isActive: false,
+      picPath: 'assets/images/john.png'
+    },
+  ];
+
+  getEmployees(): Employee[] {
+    return this.listEmployees;
+  }
+}
--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../models/employee.model';
+import { EmployeeService } from './employee.service';
 
 @Component({
   // router is used, selector can be removed.
@@ -9,45 +10,12 @@ import { Employee } from '../models/employee.model';
 })
 export class ListEmployeesComponent implements OnInit {
 
-  // hardcoding temporarily
-  employees: Employee[] = [
-    {
-      id: 1,
-      name: 'Mark',
-      gender: 'Male',
-      contactPreference: 'Email',
-      email: 'mark@test.com',
-      dateOfBirth: new Date('10/25/1978'),
-      department: 'IT',
-      isActive: true,
-      photoPath: 'assets/images/mark.png'
-    },
-    {
-      id: 2,
-      name: 'Mary',
-      gender: 'Female',
-      contactPreference: 'Phone',
-      phoneNumber: 1234431,
-      dateOfBirth: new Date('11/20/1989'),
-      department: 'HR',
-      isActive: true,
-      photoPath: 'assets/images/mary.png'
-    },
-    {
-      id: 3,
-      name: 'John',
-      gender: 'Male',
-      contactPreference: 'Phone',
-      phoneNumber: 8127121,
-      dateOfBirth: new Date('3/25/1985'),
-      department: 'IT',
-      isActive: false,
-      photoPath: 'assets/images/john.png'
-    },
-  ];
-  constructor() { }
+  employees: Employee[]=[];
+  constructor(private empService:EmployeeService) {
+   }
 
   ngOnInit() {
+    this.employees= this.empService.getEmployees();
   }
 
 }
