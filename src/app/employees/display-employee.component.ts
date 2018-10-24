8589844774007ecb859aad764e9b517a659319a5
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-10-24 22:17:09 +1100

    Angular input change detection using property setter

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -9,7 +9,22 @@ import { Employee } from '../models/employee.model';
 export class DisplayEmployeeComponent implements OnInit, OnChanges {
   // Parent component will use this Input property to pass
   // the employee object to which the template binds to
-  @Input() employee: Employee;
+  // @Input() employee: Employee;
+// Private backing field for the property
+private _employee: Employee;
+
+// This property setter is called anytime the input property changes
+@Input()
+set employee(val: Employee) {
+  console.log('Previous : ' + (this._employee ? this._employee.name : 'NULL'));
+  console.log('Current : ' + val.name);
+  this._employee = val;
+}
+
+// This getter is called when reading and displaying data
+get employee(): Employee {
+  return this._employee;
+}
 
   constructor() { }
 
@@ -17,11 +32,13 @@ export class DisplayEmployeeComponent implements OnInit, OnChanges {
   }
   // This life cycle hook receives SimpleChanges as an Input parameter. 
   // We can use it to retrieve previous and current values fro parent component
-  ngOnChanges(changes: SimpleChanges) {
-      const previousEmployee = <Employee>changes.employee.previousValue;
+   ngOnChanges(changes: SimpleChanges) {
+  /*    const previousEmployee = <Employee>changes.employee.previousValue;
       const currentEmployee = <Employee>changes.employee.currentValue;
     
       console.log('Previous : ' + (previousEmployee ? previousEmployee.name : 'NULL') );
       console.log('Current : ' + currentEmployee.name);
     }
+    */
+   
 }
