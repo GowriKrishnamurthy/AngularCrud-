ddc85e656bf357657c43055d643281d30cf1288e
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-10-24 19:13:17 +1100

    Custom validator changed to perform group field validation

--- src/app/shared/confirm-equal-validator.directive.ts
+++ src/app/shared/confirm-equal-validator.directive.ts
@@ -1,24 +1,21 @@
-import {Input, Directive } from '@angular/core';
-import  { Validator, AbstractControl, NG_VALIDATORS }  from  '@angular/forms';
+import { Input, Directive } from '@angular/core';
+import { Validator, AbstractControl, NG_VALIDATORS } from '@angular/forms';
 
 @Directive({
   selector: '[appConfirmEqualValidator]',
-  providers: [
-    {
-      provide: NG_VALIDATORS,
-      useExisting: ConfirmEqualValidatorDirective,
-      multi:  true
-    }]
-
+  providers: [{
+    provide: NG_VALIDATORS,
+    useExisting: ConfirmEqualValidatorDirective,
+    multi: true
+  }]
 })
-export class ConfirmEqualValidatorDirective implements  Validator {
-
-  @Input() appConfirmEqualValidator: string;
-  
-  validate(control: AbstractControl): { [key: string]: any } | null {
-    const controlToCompare = control.parent.get(this.appConfirmEqualValidator);
-    if (controlToCompare && controlToCompare.value !== control.value) {
-        return { 'notEqual': true };
+export class ConfirmEqualValidatorDirective implements Validator {
+  validate(passwordGroup: AbstractControl): { [key: string]: any } | null {
+    const passwordField = passwordGroup.get('password');
+    const confirmPasswordField = passwordGroup.get('confirmPassword');
+    if (passwordField && confirmPasswordField &&
+      passwordField.value !== confirmPasswordField.value) {
+      return { 'notEqual': true };
     }
 
     return null;
