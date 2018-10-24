a6b417ecfe25e23f101dc887ab3be7e520542d0e
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-10-24 22:01:30 +1100

    ngOnChanges life cycle hook: Detecting and reacting to Input prop changes

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, OnChanges } from '@angular/core';
 import { Employee } from '../models/employee.model';
 
 @Component({
@@ -6,7 +6,7 @@ import { Employee } from '../models/employee.model';
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.css']
 })
-export class DisplayEmployeeComponent implements OnInit {
+export class DisplayEmployeeComponent implements OnInit, OnChanges {
   // Parent component will use this Input property to pass
   // the employee object to which the template binds to
   @Input() employee: Employee;
@@ -15,5 +15,13 @@ export class DisplayEmployeeComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  // This life cycle hook receives SimpleChanges as an Input parameter. 
+  // We can use it to retrieve previous and current values fro parent component
+  ngOnChanges(changes: SimpleChanges) {
+      const previousEmployee = <Employee>changes.employee.previousValue;
+      const currentEmployee = <Employee>changes.employee.currentValue;
+    
+      console.log('Previous : ' + (previousEmployee ? previousEmployee.name : 'NULL') );
+      console.log('Current : ' + currentEmployee.name);
+    }
 }
