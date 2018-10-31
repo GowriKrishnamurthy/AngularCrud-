f73434f21314d06619e639e3a4f054dbdba004f7
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-10-31 18:52:35 +1100

    Data filter without custom pipes

--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../models/employee.model';
 import { EmployeeService } from './employee.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -14,12 +14,27 @@ export class ListEmployeesComponent implements OnInit {
   listEmployees: Employee[] = [];
   employeeToDisplay: Employee;
   dataFromChild: Employee;
-  searchQueryString: string;
+  _searchQueryString: string;
+
+  // Use this property to stored filtered employees, so we do not
+  // lose the original list and do not have to make a round trip
+  // to the web server on every new search
+  filteredEmployees: Employee[];
 
   constructor(private empService: EmployeeService,
-    private router: Router) {
-  }
+    private router: Router,
+    private _route: ActivatedRoute) { }
 
+  // We are binding to this property in the view template, so this
+  // getter is called when the binding needs to read the value
+  get searchQueryString(): string {
+    return this._searchQueryString;
+  }
+  // This setter is called everytime the value in the search text box changes
+  set searchQueryString(value: string) {
+    this._searchQueryString = value;
+    this.filteredEmployees = this.filterEmployees(value);
+  }
   handleNotify(eventData: Employee) {
     this.dataFromChild = eventData;
   }
@@ -27,9 +42,14 @@ export class ListEmployeesComponent implements OnInit {
     this.listEmployees = this.empService.getEmployees();
 
     // Show first employee always
-    this.employeeToDisplay = this.listEmployees[0];
+    //this.employeeToDisplay = this.listEmployees[0];
+    this.filteredEmployees = this.listEmployees;
   }
 
+  filterEmployees(searchString: string) {
+    return this.listEmployees.filter(employee =>
+      employee.name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1);
+  }
   nextEmployee(): void {
     if (this.employeeToDisplay.id <= this.listEmployees.length - 1) {
       this.employeeToDisplay = this.listEmployees[this.employeeToDisplay.id];
